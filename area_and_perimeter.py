bd211861242078fde9ceb6266b35d5ba96f6c899
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-09-12 17:28:53 +0000

    Calculates perimeter and area in C++

---
+++ area_and_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Teddy Sannan
+# Created on: September 12
+# This program calculates the area and perimeter of a rectangle
+#     with dimensions 5cm x 3cm
+
+
+def main():
+    print("If rectangle has the dimensions")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(3*5))
+    print("Perimeter is {}cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
